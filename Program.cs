9b05a728aee131e62f47ac15a8f02b9f193c9d2a
Dmitry <fedchenko.dmitry.v@gmail.com>
2015-12-02 11:57:37 +0200

    Start work with project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Traffic_Light.Modules;
+
+namespace Traffic_Light
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoTrafficLight autoTrafficLight =new AutoTrafficLight();
+            autoTrafficLight.Work();
+            Console.ReadKey();
+        }
+    }
+}
