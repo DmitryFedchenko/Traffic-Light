f262ecfdfec22b2a01e4864f5f5ffbc8e5811256
Dmitry <fedchenko.dmitry.v@gmail.com>
2015-12-02 18:27:58 +0200

    Added traffic light to crossroad

--- Modules/AutomobileTrafficLight.cs
+++ Modules/AutomobileTrafficLight.cs
@@ -2,69 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Traffic_Light.Modules
 {
     class AutomobileTrafficLight : ITrafficLight
     {
-        private string lampTop;
-        private string lampMiddle;
-        private string lampBottom;
+        public string LightKey { get; set; } = "*";
 
-        public string LampTop
-        {
-            get { return lampTop; }
-            private set { lampTop = value; }
-        }
+        public string DefualtLightKey { get; set; } = " ";
 
-        public string LampMiddle
-        {
-            get { return lampMiddle; }
-            private set { lampMiddle = value; }
-        }
+        public int[] possition = new int[6];
 
-        public string LampBottom
+       
+
+        public AutomobileTrafficLight(int lampTopX, int lampTopY, int lampMiddleX,int lampMiddleY, int lampBottomX, int lampBottomY)
         {
-            get { return lampBottom; }
-            private set { lampBottom = value; }
+            possition[0] = lampTopX;
+            possition[1] = lampTopY;
+            possition[2] = lampMiddleX;
+            possition[3] = lampMiddleY;
+            possition[4] = lampBottomX;
+            possition[5] = lampBottomY;
         }
+       
 
 
         public void SwithSignal( SignalColors colorSignalColors, int timePause)
         {
+
             switch (colorSignalColors)
             {
                 case SignalColors.Green:
-                    LampBottom = "Green";
+                    Light(LightKey,possition[4],possition[5],ConsoleColor.Green, timePause,true);
                     break;
 
                 case SignalColors.Yellow:
-                    LampMiddle = "Yellow";
+                    Light(LightKey, possition[2], possition[3], ConsoleColor.Yellow, timePause,true);
                     break;
 
                 case SignalColors.Red:
-                    LampTop = "Red";
+                    Light(LightKey, possition[0], possition[1], ConsoleColor.Red, timePause,true);
                     break;
 
                 case SignalColors.RedAndYelow:
-                    LampMiddle = "Yelow";
-                    LampBottom = "Red";
+                    Light(LightKey, possition[0], possition[1], ConsoleColor.Red, 0, false);
+                    Light(LightKey, possition[2], possition[3], ConsoleColor.Yellow, timePause,true);
+                    
                     break;
-                
-            }
+             }
+            
         }
 
+        public void Light(string key, int x, int y, ConsoleColor color, int time, bool setDefualtLight)
+        {
+             try
+            {
+                Console.ForegroundColor = color;
+                Console.SetCursorPosition( x,  y);
+                Console.Write(key);
+                Thread.Sleep(time);
+                Console.ResetColor();
+                if (setDefualtLight)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(DefualtLightKey);
 
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+            }
+        
+    }
         public void Work()
         {
             while (true)
             {
-                SwithSignal(SignalColors.Red,3000);
-                SwithSignal(SignalColors.RedAndYelow,2000);
-                SwithSignal(SignalColors.Green,3000 );
-                BlinkSignal(SignalColors.Green, 2 , 500);
-                SwithSignal(SignalColors.Yellow, 2000);
+                SwithSignal(SignalColors.Red,5000);
+                SwithSignal(SignalColors.RedAndYelow,5000);
+                SwithSignal(SignalColors.Green,5000 );
+                BlinkSignal(SignalColors.Green, 10 , 500);
+                SwithSignal(SignalColors.Yellow, 5000);
             }
         }
 
@@ -75,6 +97,7 @@ namespace Traffic_Light.Modules
             for (int i = 0; i < quantity; i++)
             {
                 SwithSignal(colorSignal, period);
+                Thread.Sleep(period);
             }
           
          }
--- Program.cs
+++ Program.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Traffic_Light.Modules;
 
 namespace Traffic_Light
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
+            AutomobileTrafficLight trafficLight = new AutomobileTrafficLight(19,9,19,10,19,11);
+            Draw drawDesk = new Draw(trafficLight);
+            drawDesk.CrossroadsWork();
+            Console.ReadLine();
 
+            /*AutomobileTrafficLight trafficLight = new AutomobileTrafficLight();
+            trafficLight.Work(); */
         }
     }
 }
